1a20bbc6c9f3ae1125c3b4c9545a6a3ed9e34077
Alvin Cennanda <alvincennanda@gmail.com>
2025-12-07 20:18:07 +0700

    fix: ui sidebar scroll

--- resources/views/layouts/navbars/auth/sidenav.blade.php
+++ resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -1,5 +1,5 @@
-<aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4"
-    id="sidenav-main" style="overflow: hidden !important;">
+<aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 h-100"
+    id="sidenav-main" style="overflow-y:auto; height: 100vh">
     <div class="sidenav-header">
         <i class="fas fa-times p-1 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
@@ -18,7 +18,8 @@
         </div>
     </div>
     <hr class="horizontal-dark mt-0">
-    <div class="collapse navbar-collapse" style="height: 100% !important; overflow-y: auto !important;">
+    {{-- <div class="collapse navbar-collapse" style="height: 100% !important; overflow-y: auto !important;"> --}}
+    <div class="collapse navbar-collapse  overflow-hidden">
         <ul class="navbar-nav">
             <li class="nav-item">
                 <a href="{{ route('dashboard') }}" class="nav-link {{ request()->is('dashboard*') ? 'active' : '' }}">
